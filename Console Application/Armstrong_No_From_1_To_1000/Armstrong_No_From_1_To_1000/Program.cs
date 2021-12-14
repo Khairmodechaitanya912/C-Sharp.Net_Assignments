6a29e52ace8aaf83ab5e3ea6d5bcab513103632d
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2021-12-14 20:40:57 +0530

    Armstrong Numbers 1 to 1000

---
+++ Console Application/Armstrong_No_From_1_To_1000/Armstrong_No_From_1_To_1000/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Armstrong_No_From_1_To_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, Sum, R, N1, N2;
+
+            for (N = 1; N <= 1000; N++)
+            {
+                N1 = N;
+                N2 = N;
+                Sum = 0;
+
+                while (N2 > 0)
+                {
+                    R = N2 % 10;
+                    Sum = Sum + (R * R * R);
+                    N2 = N2 / 10;
+                }
+
+                if (N1 == Sum)
+                {
+                    Console.WriteLine(N1);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
