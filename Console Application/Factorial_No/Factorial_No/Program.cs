450cc178093440904b11abc88804378cd6cf1d8c
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2021-12-14 20:42:48 +0530

    Factorial Numbers From 1 to 10

---
+++ Console Application/Factorial_No/Factorial_No/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Factorial_No
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n, f, i;
+
+            for (n = 1; n <= 10; n++)
+            {
+                f = 1;
+                i = n;
+
+                while (i >= 1)
+                {
+                    f = f * i;
+                    i--;
+                }
+
+                Console.WriteLine("Fact = " + f);
+                Console.ReadLine();
+            }
+        }
+    }
+}
